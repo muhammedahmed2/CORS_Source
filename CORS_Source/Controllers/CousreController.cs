e62d4bbf7bcb198e21074ba64ab0519f3fb89d05
muhammedahmed1 <mc.ahmed.khan@gmail.com>
2022-03-10 10:16:43 -0500

    Add project files.

---
+++ CORS_Source/Controllers/CousreController.cs
@@ -0,0 +1,27 @@
+﻿using CORS_Source.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CORS_Source.Controllers
+{
+    [ApiController]
+    [Route("/api/Course")]
+    public class CousreController : ControllerBase
+    {
+      
+        private CourseService _course_service;
+
+        public CousreController(CourseService _svc)
+        {
+            _course_service = _svc;
+
+        }
+
+        [HttpGet]
+        public IActionResult GetCourses()
+        {
+            return Ok(_course_service.GetCourses());
+        }
+      
+    }
+}
+
---
+++ CORS_Source/Services/CourseService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using CORS_Source.Models;
+
+namespace CORS_Source.Services
+{
+    public class CourseService
+    {
+        public List<Course> course_lst;
+
+        public CourseService()
+        {
+
+            course_lst = new List<Course>();
+            {
+                new Course() { CourseID = 1, CourseName = "Az-204" };
+                new Course() { CourseID = 2, CourseName = "Az-404" };
+                new Course() { CourseID = 1, CourseName = "Az-304" };
+            };
+
+
+        }
+
+        public IEnumerable<Course> GetCourses()
+        {
+            return (course_lst);
+        }
+    }
+ }
